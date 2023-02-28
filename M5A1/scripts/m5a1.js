f4c1f985e17bda23c6508061622e76ec1afe02f0
NewbRangerTom <67010348+NewbRangerTom@users.noreply.github.com>
2023-02-28 17:10:43 -0500

    working through the javascript function

--- M5A1/scripts/m5a1.js
+++ M5A1/scripts/m5a1.js
@@ -2,31 +2,30 @@
 // Author:     Thomas Werstlein
 // Date:       Mar 2023 
 
-function prompt(){
-    tday = new Array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday","Saturday");
-    tmonth = new Array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
-    tline = new String("Current Date/Time:&nbsp;&nbsp;");
-
-    day = new Date();
-    nday = day.getDay();
-    nmonth = day.getMonth();
-    ndate = day.getDate();
-    nyear = day.getYear();
-    nhour = day.getHours();
-    nmin = day.getMinutes();
-    nsec = day.getSeconds();
-
-    if(nyear < 1000) nyear = nyear + 1900;
-
-    if(nhour == 0){ap = " AM"; nhour = 12;}
-    else if(nhour <= 11){ap = " AM";}
-    else if(nhour == 12){ap = " PM";}
-    else if(nhour >= 13){ap = " PM"; nhour -= 12;}
-
-    if(nmin <= 9){nmin = "0" + nmin;}
-    if(nsec <= 9){nsec = "0" + nsec;}
-
-    document.getElementById('GetClock').innerHTML = ""+tline+" "+tday[nday]+", "+tmonth[nmonth]+" "+ndate+", "+nyear+" "+nhour+":"+nmin+":"+nsec+ap+"";
-    setTimeout("GetClock()", 1000);
+function Prompt(){
+    var text = "";
+    var HITS = 0;
+    var UI = prompt("");
+    var TV = parseInt(UI);
+    var DD = TV;
+    while (DD > 0){
+        if (TV % DD == 0) {
+            HITS += 1;
+            DD -= 1;
+            if (HITS > 2) {
+                text = UI + " is not a prime number";
+              }
+            else {
+                text = UI + " is a prime number";
+              }
+            }   
+        }
+    document.getElementById("Prompt").innerHTML = text;
+    document.getElementById("Prompt").style.setProperty("background-color", "lightgrey");
+    // document.getElementById("Prompt")
+    // document.getElementById("Prompt")
+    // document.getElementById("Prompt")
+    // document.getElementById("Prompt")
+    // document.getElementById("Prompt")
 }
-window.onload = prompt;
+window.prompt("Script Prompt:\nEnter a whole number to test as a prime number:")
