7c2273bb993159dfc74f382528fac48fb70e52c4
NewbRangerTom <67010348+NewbRangerTom@users.noreply.github.com>
2023-02-28 13:59:39 -0500

    renamed module2 to M2A1 to conform with naming created m5a1.js started adding content to m5a1.html

---
+++ M5A1/scripts/m5a1.js
@@ -0,0 +1,32 @@
+// Assignment M5A1
+// Author:     Thomas Werstlein
+// Date:       Mar 2023 
+
+function prompt(){
+    tday = new Array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday","Saturday");
+    tmonth = new Array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
+    tline = new String("Current Date/Time:&nbsp;&nbsp;");
+
+    day = new Date();
+    nday = day.getDay();
+    nmonth = day.getMonth();
+    ndate = day.getDate();
+    nyear = day.getYear();
+    nhour = day.getHours();
+    nmin = day.getMinutes();
+    nsec = day.getSeconds();
+
+    if(nyear < 1000) nyear = nyear + 1900;
+
+    if(nhour == 0){ap = " AM"; nhour = 12;}
+    else if(nhour <= 11){ap = " AM";}
+    else if(nhour == 12){ap = " PM";}
+    else if(nhour >= 13){ap = " PM"; nhour -= 12;}
+
+    if(nmin <= 9){nmin = "0" + nmin;}
+    if(nsec <= 9){nsec = "0" + nsec;}
+
+    document.getElementById('GetClock').innerHTML = ""+tline+" "+tday[nday]+", "+tmonth[nmonth]+" "+ndate+", "+nyear+" "+nhour+":"+nmin+":"+nsec+ap+"";
+    setTimeout("GetClock()", 1000);
+}
+window.onload = prompt;
