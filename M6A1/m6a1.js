a88d2fb0e271dd9b8f72563b93a696742a6954b8
NewbRangerTom <67010348+NewbRangerTom@users.noreply.github.com>
2023-03-17 14:21:21 -0400

    created files for assignment m6a1.

---
+++ M6A1/m6a1.js
@@ -0,0 +1,43 @@
+// File name:	m6a1.js
+// Author:		Thomas Werstlein
+// Date:		April 2023
+
+// M6A1 Project Constraints F-a
+// Browser Test
+function browserTest(){
+
+}
+
+// M6A1 Project Constraints F-b
+// HTML Feature Test
+function featureTest(){
+
+}
+
+// M6A1 Project Constraints F-c
+// Mobile Tests
+function resolution(){
+
+}
+
+function orientation(){
+
+}
+
+// M6A1 Project Constraints F-d
+// Canvas Tests
+function genSup(){
+
+}
+
+function textSup(){
+
+}
+
+function drawCanvas(){
+	var cnvs = document.getElementById("myCanvas");
+	var ctxt = cnvs.getContext("2d");
+	// var img = document.getElementById("scream");
+	var img = "../images/transgender-symbol.png";
+	ctx.drawImage(img, 10, 10);
+}
