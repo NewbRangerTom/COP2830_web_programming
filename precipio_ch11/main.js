ec07ef57bcc2d7c04d566a7210c4e2f236591ac5
NewbRangerTom <67010348+NewbRangerTom@users.noreply.github.com>
2023-04-19 10:48:54 -0400

    added description comments to help explain what's happening

--- precipio_ch11/main.js
+++ precipio_ch11/main.js
@@ -2,13 +2,15 @@
 function showCode(parentElement){
     var parentElementDiv = document.getElementById(parentElement);
     var exampleElement = document.querySelector('#' + parentElement + ' .example');
-
+    // first check if section has class .open, if true close section
     if (parentElementDiv.className == 'section open') {
         var codeBlock = exampleElement.querySelector('.example-code');
         exampleElement.removeChild(codeBlock);
         document.querySelector('#' + parentElement + ' .code-show-button').value = 'Show Code';
         parentElementDiv.classList.remove('open');
-    } else {
+    } 
+    // if the section does not have class .open, create and show the section
+    else {
         var text = exampleElement.innerHTML;
         var code = document.createTextNode(text);
         var codeElement = document.createElement('div');
